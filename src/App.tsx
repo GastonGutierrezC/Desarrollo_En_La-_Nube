998ea779cfd25872ffddb56c94f347cb8ebf97e4
gaston <gaston.gutierrez.9964359@usalesiana.edu.bo>
2025-06-06 10:47:10 -0400

    initial proyect

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+
+
+import { onAuthStateChanged } from "firebase/auth";
+
+const App: React.FC = () => {
+
+
+
+  return (
+    <Router>
+      <Routes>
+        
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
